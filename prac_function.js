d439c681625f287a2028eef0ad0206de654b40d9
ASTHA MORE <asthauday2004@gmail.com>
2025-08-03 22:37:41 +0530

    functions commit

---
+++ prac_function.js
@@ -0,0 +1,8 @@
+function calculatesquare(num){
+let value= num * num;
+return value;}
+//calculatesquare(5);
+//console.log("Square of 5 is: " + calculatesquare(5));
+result = calculatesquare(10);
+console.log("Square of 10 is: " + result);
+console.log("This is a test file for the fortest.js path.");
